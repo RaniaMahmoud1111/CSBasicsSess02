42f6850a68ed3bca5283133ba429b777d31168ab
Rania Mahmoud <Rania.Mahm555@gmail.com>
2024-12-07 12:16:53 +0200

    Add project files.

---
+++ CSBasicsSess02/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        #region q 01:	Write a program that allows the user to enter a number then print it.
+
+        //Console.WriteLine("hello, please enter a number ");
+        // string inputNum=Console.ReadLine();
+        //Console.WriteLine($"the number you have enter is : {inputNum}");
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
