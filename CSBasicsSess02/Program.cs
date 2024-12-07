b63a599a91e2e8a474aed372bf26e58ca580a540
Rania Mahmoud <Rania.Mahm555@gmail.com>
2024-12-07 12:23:21 +0200

    add question 01

--- CSBasicsSess02/Program.cs
+++ CSBasicsSess02/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
 
-        #region q 01:	Write a program that allows the user to enter a number then print it.
+        #region q 01: Write a program that allows the user to enter a number then print it.
 
         //Console.WriteLine("hello, please enter a number ");
         // string inputNum=Console.ReadLine();
